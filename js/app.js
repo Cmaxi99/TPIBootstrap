ea53311799753ca7a9729d1c4433e97712b9cb0c
Cmaxi99 <maxicanto2009@hotmail.com>
2022-05-24 17:19:39 -0300

    Se agrega restriccion de que el formulario debe estar completo

--- js/app.js
+++ js/app.js
@@ -1,27 +1,37 @@
-function valor(){
-    const ticket = 200;
-    let cant = document.getElementById('cantidad').value;
-    let categoria = document.getElementById('categoria').value;
+const ticket = 200;
+
+function valor() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let cant = document.getElementById("cantidad").value;
+    let categoria = document.getElementById("categoria").value;
     let bruto = cant * ticket;
     let descuento = 0;
-    switch (categoria) {
-        case "Estudiante":
-            descuento = 0.8;
-            break;
-        case "Trainee":
-            descuento = 0.5;
-            break;
-        case "Junior":
-            descuento = 0.15;
-            break;
-    };
+    if(nombre != "" && apellido != "" && email != "" && cant != ""){
+        switch (categoria) {
+            case "Estudiante":
+                descuento = 0.8;
+                break;
+            case "Trainee":
+                descuento = 0.5;
+                break;
+            case "Junior":
+                descuento = 0.15;
+                break;
+        }
 
-    let total = bruto - (bruto * descuento);
+        let total = bruto - bruto * descuento;
 
-    document.getElementById(
-    "pagar"
-    ).value = `Total a Pagar: $ ${total} pesos`;
+        document.getElementById(
+        "pagar"
+        ).value = `Total a Pagar: $ ${total} pesos`;
 
-    alert(`Usted debera pagar un total de: $ ${total} pesos`);
+        alert(`${nombre}: usted debera pagar un total de: $ ${total} pesos.`);
+    }
+    else{
+        alert(`Debe completar el formulario`);
+    }
+
     
 }
